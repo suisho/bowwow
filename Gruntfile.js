26642c72dae651d4788e1d75ad51333180ad6d15
suisho <suisho000000@gmail.com>
2014-12-01 15:56:05 +0900

    zip

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+
+module.exports = function(grunt) {
+  
+  grunt.initConfig({
+    'build-atom-shell-app': {
+      options: {
+        platforms: ["darwin", "win32"]
+      }
+    },
+    zip : {
+      'release.zip' : 'build'
+    },
+  })
+  grunt.loadNpmTasks('grunt-atom-shell-app-builder')
+  grunt.loadNpmTasks('grunt-zip')
+  
+  grunt.registerTask("default",['build-atom-shell-app', 'zip'])
+}
