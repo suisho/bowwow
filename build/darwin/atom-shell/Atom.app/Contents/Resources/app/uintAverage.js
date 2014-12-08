0caadb9b871fa2e3fc4e87a4a23f3d21ec55c24d
suisho <suisho000000@gmail.com>
2014-12-08 22:12:39 +0900

    remove

--- build/darwin/atom-shell/Atom.app/Contents/Resources/app/uintAverage.js
+++
@@ -1,8 +0,0 @@
-module.exports = function uintAverage(array){
-  var sum = 0
-  var length = array.length 
-  for(var i = 0; i < length; i++){
-    sum += array[i]
-  }
-  return sum / length
-}
--- build/win32/atom-shell/resources/app/lib/normalize-value.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(x, y, maxX, maxY){
-  var normX = x / maxX
-  var normY = y / maxY
-  return [normX, normY]
-}
